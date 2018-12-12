2939c3a258c1c6ce7565fa3d848d69358838725c
dasenlinCode <2845293434@qq.com>
2018-12-12 14:50:30 +0800

    add eth coin

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var bitcore = module.exports;
 bitcore.version = 'v' + require('./package.json').version;
 bitcore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib found. ' +
-      'Please make sure to require bitcore-lib and check that submodules do' +
-      ' not also include their own bitcore-lib dependency.';
+    var message = 'More than one instance of bitcore-lib-eth found. ' +
+      'Please make sure to require bitcore-lib-eth and check that submodules do' +
+      ' not also include their own bitcore-lib-eth dependency.';
     throw new Error(message);
   }
 };
-bitcore.versionGuard(global._bitcore);
-global._bitcore = bitcore.version;
+bitcore.versionGuard(global._bitcoreEth);
+global._bitcoreEth = bitcore.version;
 
 // crypto
 bitcore.crypto = {};
